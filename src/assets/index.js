51764987d0c5561a156afc3888e91ab9a171a3c7
chinenzl <chinenzl@163.com>
2023-12-04 00:34:12 +1300

    complete

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
+import wms from "./company/wms.png";
 import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import gelin from "./company/gelin.png";
+import SnapInspect from "./company/SnapInspect.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +52,10 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
+  wms,
   shopify,
-  starbucks,
-  tesla,
+  gelin,
+  SnapInspect,
   carrent,
   jobit,
   tripguide,
